4c97f43736a238436b457afc98e95653c164a678
tyulyukov <makstyulyukov@gmail.com>
2022-10-06 19:28:22 +0300

    Initial commit

---
+++ src/test/java/tyulyukov/resumebuilderspring/ResumeBuilderSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package tyulyukov.resumebuilderspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeBuilderSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
